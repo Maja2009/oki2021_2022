d1b0c7fed107caf9509d851513dcf45054620757
Maja2009 <102176813+Maja2009@users.noreply.github.com>
2022-03-22 19:27:38 +0100

    Add files via upload

---
+++ Buźka.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "    ////";
+cout << "\n";
+cout << " ( O   O )";
+cout << "\n";
+cout << "----(_)----";
+cout << "\n";
+
+	return 0;
+}
